e30f1aaf073048b1e43c35ea05998aefa85fc8f2
pangphaichit <pg_grj@hotmail.com>
2024-10-23 13:50:09 +0700

    feat: add greeting message buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setgreetingMessage] = useState("")
+  const handleSawadeeClick = () => {
+    setgreetingMessage("สวัสดี!")
+  }
+
+  const handleHiClick = () => {
+    setgreetingMessage("Hi!")
+  }
+
+  const handleNihaoClick = () => {
+    setgreetingMessage("你好!")
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container"> {greetingMessage ? greetingMessage : "Greeting Message"} </div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleSawadeeClick}>สวัสดี!</button>
+        <button onClick={handleHiClick}>Hi!</button>
+        <button onClick={handleNihaoClick}>你好!</button>
       </div>
     </div>
   );
